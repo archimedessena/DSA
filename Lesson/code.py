82a25b1fe897be457c3d1c2c1234a0146c60cc50
Archimedes Sena Senadju <37110385+archimedessena@users.noreply.github.com>
2022-04-22 10:26:03 +0000

    hashtable

--- Lesson/code.py
+++ Lesson/code.py
@@ -1,15 +1,25 @@
-numbers = [1, 2, 3, 9] # 9
+# merge sorted array
+# function to merge a sorted array
 
-numberss = [1, 2, 4, 4] # 8
-
-def pair(arr, target):
-    for i in arr:
-        for j in arr:
-            if i + j == target:
-                return i, j 
-            else:
-                return None
-    return i, j
+# merge sorted array frm 
 
 
-print(pair([1, 2, 3, 9], 4))
+def merge_sorted_array(arr, arrr):
+    if len(arr) == 0 and len(arrr) == 0:
+        return arr + arrr
+    # if that is not the case then this
+    arrs = []
+    num = 0 # init
+    nums = 0   # init
+    while num < len(arr) and nums < len(arrr):
+        if arr[num] <= arrr[nums]:
+            arrs.append(arr[num])
+            num+=1   
+        elif  arrr[nums] < arr[num]:
+            arrs.append(arrr[nums])
+            nums+=1
+    return arrs
+
+
+sorted_array = merge_sorted_array([1, 2, 3, 4, 5, 6,], [7, 8, 9])
+print(sorted_array)
