cb96a5691eb456c414a256cd93a2f762c44a39c6
Archimedes Sena Senadju <37110385+archimedessena@users.noreply.github.com>
2022-07-03 12:02:28 +0000

    leetcode problems added

---
+++ .history/Lesson/count_20220701201248.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        if low % 2 !=0 and high % 2 != 0:
+            count = ((high-low+1)//2) + 1
+        else:
+            count = ((high-low+1)//2)
+        return count
+    
+    
+    
+    
+    
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        return (high-low+1)//2+(high%2+low%2)//2
---
+++ .history/Lesson/count_20220703095306.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        if low % 2 !=0 and high % 2 != 0:
+            count = ((high-low+1)//2) + 1
+        else:
+            count = ((high-low+1)//2)
+        return count
+    
+    
+test = Solution()
+tet1 = test.countOdds(3, 15)
+print(tet1)
+    
+# class Solution:
+#     def countOdds(self, low: int, high: int) -> int:
+#         return (high-low+1)//2+(high%2+low%2)//2
